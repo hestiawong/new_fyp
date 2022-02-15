170914f5dbfb8e1529eca2725137b7b9195c2eca
hestiawong <g1224149@study.hkmu.edu.hk>
2022-02-15 15:18:16 +0800

    update 220215 15:18

--- app/src/main/java/com/example/fyp_app/openfoodfact.java
+++ app/src/main/java/com/example/fyp_app/openfoodfact.java
@@ -23,6 +23,7 @@ import com.google.zxing.integration.android.IntentResult;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 
@@ -30,6 +31,8 @@ public class openfoodfact extends AppCompatActivity {
 
     private TextView foodname_textView;
     private TextView ingred_textView;
+    private TextView quan_textView;
+    private ImageView nutri_imageView;
     private RequestQueue mQueue;
 
     @Override
@@ -38,13 +41,14 @@ public class openfoodfact extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         foodname_textView = findViewById(R.id.snap_food);
         ingred_textView = findViewById(R.id.nutri_descri);
+        quan_textView = findViewById(R.id.snap_quan);
+        nutri_imageView = findViewById(R.id.snap_nutri);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
 
         mQueue = Volley.newRequestQueue(this);
     }
 
     public void ScanButton(View view){
-
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.initiateScan();
     }
@@ -55,6 +59,7 @@ public class openfoodfact extends AppCompatActivity {
         if(intentResult!=null){
             if(intentResult.getContents()==null){
                 foodname_textView.setText("Not found");
+                quan_textView.setText("Not found");
                 ingred_textView.setText("Not found");
             }
             else{
@@ -73,9 +78,13 @@ public class openfoodfact extends AppCompatActivity {
                     JSONObject productname = response.getJSONObject("product");
                     String productnameString = productname.getString("product_name");
                     String ingredientsString = productname.getString("ingredients_text");
+                    String quantityString = productname.getString("product_quantity");
+                    String imgurl = productname.getString("image_nutrition_url");
 
                     foodname_textView.setText(productnameString);
                     ingred_textView.setText(ingredientsString);
+                    quan_textView.setText(quantityString);
+                    Picasso.get().load(imgurl).into(nutri_imageView);
 
                 }
                 catch(Exception e){
